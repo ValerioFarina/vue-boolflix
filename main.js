da471d0d24080158f1c27c005741bb0ffd8db6d0
Valerio Farina <valerio.farina.1994@gmail.com>
2020-11-24 15:42:40 +0100

    Milestone 2: sostituito il voto numerico con il voto in stelline

--- main.js
+++ main.js
@@ -10,17 +10,26 @@ var app = new Vue({
 
     methods: {
         getSearchResult() {
-            axios
-                .get('https://api.themoviedb.org/3/search/movie', {
-                    params: {
-                        api_key: '8762c3f242ebc4064f2c46af1dbdebc0',
-                        query: this.searched
-                    }
-                })
-                .then((responseObject) => {
-                    this.searchResult = responseObject.data.results;
-                    this.loaded = true;
-                });
+            if (this.searched != '') {
+                axios
+                    .get('https://api.themoviedb.org/3/search/movie', {
+                        params: {
+                            api_key: '8762c3f242ebc4064f2c46af1dbdebc0',
+                            query: this.searched
+                        }
+                    })
+                    .then((responseObject) => {
+                        this.searchResult = responseObject.data.results;
+                        this.loaded = true;
+                    });
+            } else {
+                this.searchResult = [];
+                this.loaded = false;
+            }
+        },
+
+        toStars(vote) {
+            return Math.round((vote * 5) / 10);
         }
     },
 
