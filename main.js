6feed813d28a12dcccd52611656d248c74ba142b
Valerio Farina <valerio.farina.1994@gmail.com>
2020-11-24 18:39:02 +0100

    Inseriti film e serie tv in due array separati

--- main.js
+++ main.js
@@ -3,18 +3,27 @@ var app = new Vue({
 
     data: {
         searched : '',
-        searchResult : [],
+        searchResult : {
+            movies : [],
+            tvSeries : []
+        },
+        loaded : {
+            movies : false,
+            tvSeries : false
+        },
         baseUrl : ''
     },
 
     methods: {
-        // this function populates the array "searchResult" with objects representing movies and tv series
-        // according to the value of "searched" (which is the text written by the user in the input)
+        // this function populates the array searchResult.movies with objects representing movies
+        // and the array searchResult.tvSeries with objects representing tv series
+        // according to the value of searched (which is the text written by the user in the input)
         getSearchResult() {
-            // we reset the value of "searchResult"
-            this.searchResult = [];
-            // if the input inserted by the user is not an empty string, we make the API calls we need
+            // we reset the values of loaded.movies and loaded.tvSeries
+            this.loaded.movies = false;
+            this.loaded.tvSeries = false;
             if (this.searched != '') {
+                // if the input inserted by the user is not an empty string, we make the API calls we need
                 // we get the movies that match the search made by the user
                 axios
                     .get('https://api.themoviedb.org/3/search/movie', {
@@ -24,11 +33,13 @@ var app = new Vue({
                         }
                     })
                     .then((responseObject) => {
-                        // we add the movies to the array "searchResult"
-                        this.searchResult = [...this.searchResult, ...responseObject.data.results];
+                        // we add the movies to the array searchResult.movies
+                        this.searchResult.movies = responseObject.data.results;
+                        // we set loaded.movies equal to true
+                        this.loaded.movies = true;
                     });
 
-                // we get the tv shows that match the search made by the user
+                // we get the tv series that match the search made by the user
                 axios
                     .get('https://api.themoviedb.org/3/search/tv', {
                         params: {
@@ -37,9 +48,16 @@ var app = new Vue({
                         }
                     })
                     .then((responseObject) => {
-                        // we add the tv shows to the array "searchResult"
-                        this.searchResult = [...this.searchResult, ...responseObject.data.results];
+                        // we add the tv series to the array searchResult.tvSeries
+                        this.searchResult.tvSeries = responseObject.data.results;
+                        // we set loaded.tvSeries equal to true
+                        this.loaded.tvSeries = true;
                     });
+            } else {
+                // if the input inserted by the user is an empty string,
+                // we reset the values of searchResult.movies and searchResult.tvSeries
+                this.searchResult.movies = [];
+                this.searchResult.tvSeries = [];
             }
         },
         // this function converts a rate based on a 0-to-10 scale into a rate based on a 0-to-5 scale
