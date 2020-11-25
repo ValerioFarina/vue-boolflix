925467e5fb0f4ff8b6b1f3f13dd64d7e56fe6362
Valerio Farina <valerio.farina.1994@gmail.com>
2020-11-25 12:57:40 +0100

    Aggiunto messaggio per ricordare all'utente che cosa ha cercato

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 var app = new Vue({
-    el: '#root',
+    el : '#root',
 
-    data: {
-        searched : '',
+    data : {
+        searched : {
+            input : '',
+            title : ''
+        },
         searchResult : {
             movies : [],
             tvSeries : []
@@ -14,22 +17,26 @@ var app = new Vue({
         baseUrl : ''
     },
 
-    methods: {
+    methods : {
         // this function populates the array searchResult.movies with objects representing movies
         // and the array searchResult.tvSeries with objects representing tv series
-        // according to the value of searched (which is the text written by the user in the input)
+        // according to the value of searched.input (which is the text written by the user in the input)
         getSearchResult() {
-            // we reset the values of loaded.movies and loaded.tvSeries
-            this.loaded.movies = false;
-            this.loaded.tvSeries = false;
-            if (this.searched.trim() != '') {
-                // if the input inserted by the user is not an empty string, we make the API calls we need
+            if (this.searched.input.trim() != '') {
+                // if the input inserted by the user is not an empty string
+                // we save in searched.title the value of searched.input
+                this.searched.title = this.searched.input.trim();
+                // we reset the value of searched.input
+                this.searched.input = '';
+                // we reset the values of loaded.movies and loaded.tvSeries
+                this.loaded.movies = false;
+                this.loaded.tvSeries = false;
                 // we get the movies that match the search made by the user
                 axios
                     .get('https://api.themoviedb.org/3/search/movie', {
                         params: {
                             api_key: '8762c3f242ebc4064f2c46af1dbdebc0',
-                            query: this.searched
+                            query: this.searched.title
                         }
                     })
                     .then((responseObject) => {
@@ -44,7 +51,7 @@ var app = new Vue({
                     .get('https://api.themoviedb.org/3/search/tv', {
                         params: {
                             api_key: '8762c3f242ebc4064f2c46af1dbdebc0',
-                            query: this.searched
+                            query: this.searched.title
                         }
                     })
                     .then((responseObject) => {
@@ -53,11 +60,6 @@ var app = new Vue({
                         // we set loaded.tvSeries equal to true
                         this.loaded.tvSeries = true;
                     });
-            } else {
-                // if the input inserted by the user is an empty string,
-                // we reset the values of searchResult.movies and searchResult.tvSeries
-                this.searchResult.movies = [];
-                this.searchResult.tvSeries = [];
             }
         },
         // this function converts a rate based on a 0-to-10 scale into a rate based on a 0-to-5 scale
